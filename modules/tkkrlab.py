84f9501643281eec95e90a635957110bfe8fad78
Jasper Seidel <tyrion@jawsper.nl>
2013-02-28 19:20:04 +0100

    Added time to tkkrlab. Read space open/close time from file mtime.

--- modules/tkkrlab.py
+++ modules/tkkrlab.py
@@ -1,9 +1,14 @@
 from _module import _module
-import httplib
+
+import httplib, urllib, urlparse
+
 import datetime
 from dateutil.tz import tzlocal
 import dateutil.parser
+import time
+
 import random
+import os.path
 
 class tkkrlab( _module ):
 	def __init__( self, config ):
@@ -11,9 +16,10 @@ class tkkrlab( _module ):
 		self.space_open = None
 
 	def can_handle( self, cmd, admin ):
-		return cmd in ( 'status', 'led', 'quote', 'help' )
+		return cmd in ( 'status', 'led', 'time', 'quote', 'help' )
+
 	def handle( self, bot, cmd, args, source, target, admin ):
-		( local_status, status_date ) = self.__get_local_status()
+		( local_status, status_date ) = self.__get_space_status()
 		if cmd == 'help':
 			for line in [
 				'!quote: to get a random quote',
@@ -27,28 +33,32 @@ class tkkrlab( _module ):
 		elif cmd == 'quote':
 			bot.privmsg( target, 'Quote: ' + self.__random_quote() )
 		elif cmd == 'status':
-			bot.privmsg( target, 'We are {0}'.format( 'Open' if local_status == True else 'Closed' ) )
+			if local_status not in ( True, False ):
+				bot.privmsg( target, 'Error: {0}'.format( local_status ) )
+			else:
+				bot.privmsg( target, 'We are {0} since {1}'.format( 'Open' if local_status == True else 'Closed', datetime.datetime.fromtimestamp( status_date, tzlocal() ).strftime( '%a, %d %b %Y %H:%M:%S %Z' ) ) )
 		elif cmd == 'led':
 			if local_status == True:
-				bot.privmsg( target, 'Led ' + self.__send_led( ' '.join( args ) ) )
+				bot.privmsg( target, 'Led: {0}'.format( self.__send_led( ' '.join( args ) ) ) )
 			elif local_status == False:
 				bot.privmsg( target, 'Sorry ' + source + ', can only do this when space is open.' )
 			else:
 				bot.privmsg( target, 'Error: ' + local_status )
 		elif cmd == 'time':
-			pass
+			self.__send_led( time.strftime( '%H:%M' ).center( 16 ) )
 
-	def __get_local_status( self ):
+	def __get_space_status( self ):
 		try:
-			with open( self.statusfile ) as fd:
+			with open( self.status_file ) as fd:
 				space_opened = fd.readline().strip()
 				if self.space_open == None:
 					self.space_open = space_opened == '1'
 
 				if self.space_open != ( space_opened == '1' ):
 					self.space_open = space_opened == '1'
-					self.__set_topic( bot, '#tkkrlab', 'We zijn Open' if self.space_open else 'We zijn Dicht' )
-			return ( self.space_open, None )
+					#self.__set_topic( bot, '#tkkrlab', 'We zijn Open' if self.space_open else 'We zijn Dicht' )
+			space_date = os.path.getmtime( self.status_file )
+			return ( self.space_open, space_date )
 		except AttributeError:
 			self.space_open = 'No status file configured'
 		except IOError:
@@ -59,14 +69,31 @@ class tkkrlab( _module ):
 		bot.connection.topic( channel, new_topic )
 		bot.privmsg( channel, new_topic )
 
-	def __send_led( self, message ):
-		pass
+	def __send_led( self, message):
+		"""Send a command to the led board"""
+		try:
+			url = urlparse.urlparse( self.led_url.format( urllib.quote( message[:85] ) ) )
+			conn = httplib.HTTPConnection( url.netloc, timeout=10 )
+			conn.request( 'GET', url.path )
+			response = conn.getresponse()
+			res = response.status
+			conn.close()
+			if res != 200:
+				return 'Error:' + res + ' - ' + response.reason
+			else:
+				return 'OK'
+		except IOError, e:
+			return 'Cannot connect to LED server: "{0}"'.format( e )
+		except AttributeError:
+			return 'LED URL not set'
 
 	def __random_quote( self ):
 		"""Read a quote from a text file"""
 		try:
-			with open( "quotes.txt" ) as fd:
+			with open( self.quote_file ) as fd:
 				return random.choice( fd.readlines() )
+		except AttributeError:
+			return 'Error: no quote file defined'
 		except IOError:
-			return 'No quote file found'
+			return 'Error: quote file not found'
 
