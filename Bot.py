50dc858b7cee6ea778410b7d78868acbd72e52f1
Jasper Seidel <sammy@jawsper.nl>
2013-03-17 21:45:38 +0100

    Bot override die method, stop all modules

--- Bot.py
+++ Bot.py
@@ -54,7 +54,17 @@ class Bot( SingleServerIRCBot ):
 			except socket.timeout:
 				print( 'Socket timeout' )
 				return False
-
+	
+	def die(self):
+		if self.modules:
+			for module in self.modules:
+				try:
+					self.modules[module].stop()
+				except Exception, e:
+					print( 'Failed to stop module {0}: {1}'.format( module, e ) )
+			del self.modules
+		SingleServerIRCBot.die(self)
+		
 	def __reload_config( self ):
 		self.config.read( os.path.expanduser( "~/.ircbot" ) )
 		self.admin = self.config.get( 'main', 'admin' ).split( ';' )
