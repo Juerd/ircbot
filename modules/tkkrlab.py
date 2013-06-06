5988ef4732e0f53a63d22b8884f793ea4e9b7c20
Jasper Seidel <tyrion@jawsper.nl>
2013-06-06 15:24:09 +0200

    Adapted tkkrlab module to ModuleManager style

--- modules/tkkrlab.py
+++ modules/tkkrlab.py
@@ -36,8 +36,8 @@ class StatusMonitor( threading.Thread ):
 		self.socket.close()	
 
 class tkkrlab( _module ):
-	def __init__( self, config, bot ):
-		_module.__init__( self, config, bot )
+	def __init__( self, mgr ):
+		_module.__init__( self, mgr )
 		self.space_open_data = None
 		self.space_status = ( None, None )
 		try:
@@ -49,45 +49,46 @@ class tkkrlab( _module ):
 	def stop(self):
 		self.thread.stop()
 		self.thread.join()
-
-	def can_handle( self, cmd, admin ):
-		return cmd in ( 'status', 'led', 'time', 'quote' ) or admin and cmd in ( 'admin_help', 'force_status', 'force_topic_update' )
-
-	def handle( self, bot, cmd, args, source, target, admin ):
+	
+	def admin_cmd_force_status( self, args, source, target, admin ):
+		"""!force_status <0|1>: force space status to closed/open"""
+		if not admin: return
+		if len( args ) == 0: return
+		if args[0] in ( '0', '1' ):
+			self.set_space_status( args[0], time.time() )
+	
+	def admin_cmd_force_topic_update( self, args, source, target, admin ):
+		"""!force_topic_update: force topic update"""
+		if not admin: return
+		( local_status, ) = self.__get_space_status()
+		self.__set_topic( '#tkkrlab', 'We zijn Open' if local_status else 'We zijn Dicht' )
+		
+	def cmd_quote( self, args, source, target, admin ):
+		"""!quote: to get a random quote"""
+		return [ 'Quote: ' + self.__random_quote() ]
+		
+	def cmd_status( self, args, source, target, admin ):
+		"""!status: to get open/close status of the space"""
 		( local_status, status_date ) = self.__get_space_status()
-		if admin:
-			if cmd == 'admin_help':
-				bot.notice( source, '!force_status <status>: force a status update' )
-				bot.notice( source, '!force_topic_update: force a topic update' )
-			elif cmd == 'force_status':
-				if len( args ) == 0: return
-				if args[0] in ( '0', '1' ):
-					self.set_space_status( args[0], time.time() )
-			elif cmd == 'force_topic_update':
-				self.__set_topic( '#tkkrlab', 'We zijn Open' if self.space_open else 'We zijn Dicht' )
-				return
-		if cmd == 'help':
-			return [
-				'!quote: to get a random quote',
-				'!status: to get open/close status of the space',
-				'!led message: put message on led matrix board',
-				'!time: put current time on led matrix board',
-			]
-		elif cmd == 'quote':
-			return [ 'Quote: ' + self.__random_quote() ]
-		elif cmd == 'status':
-			if local_status not in ( True, False ):
-				return [ 'Error: status is not True/False but {0}'.format( local_status ) ]
-			else:
-				return [ 'We are {0} since {1}'.format( 'Open' if local_status == True else 'Closed', datetime.datetime.fromtimestamp( status_date, tzlocal() ).strftime( '%a, %d %b %Y %H:%M:%S %Z' ) ) ]
-		elif cmd == 'led':
-			if local_status == True:
-				return [ 'Led: {0}'.format( self.__send_led( ' '.join( args ) ) ) ]
-			elif local_status == False:
-				return [ 'Sorry ' + source + ', can only do this when space is open.' ]
-			else:
-				return [ 'Error: ' + local_status ]
-		elif cmd == 'time':
+		if local_status not in ( True, False ):
+			return [ 'Error: status is not True/False but {0}'.format( local_status ) ]
+		else:
+			return [ 'We are {0} since {1}'.format( 'Open' if local_status == True else 'Closed', datetime.datetime.fromtimestamp( status_date, tzlocal() ).strftime( '%a, %d %b %Y %H:%M:%S %Z' ) ) ]
+	
+	def cmd_led( self, args, source, target, admin ):
+		"""!led <message>: put message on led matrix board"""
+		( local_status, status_date ) = self.__get_space_status()
+		if local_status == True:
+			return [ 'Led: {0}'.format( self.__send_led( ' '.join( args ) ) ) ]
+		elif local_status == False:
+			return [ 'Sorry ' + source + ', can only do this when space is open.' ]
+		else:
+			return [ 'Error: status is not True/False but {0}'.format( local_status ) ]
+		
+	def cmd_time( self, args, source, target, admin ):
+		"""!time: put current time on led matrix board"""
+		( local_status, status_date ) = self.__get_space_status()
+		if local_status == True:
 			self.__send_led( time.strftime( '%H:%M' ).center( 16 ) )
 
 	def set_space_status( self, status, aTime ):
