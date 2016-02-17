e9569aa1dcdc45eda4a2e87eed2c8afcd225f290
Jasper Seidel <lenny@jawsper.nl>
2016-02-17 20:38:42 +0100

    Tabs to spaces.

--- modules/bofh.py
+++ modules/bofh.py
@@ -3,18 +3,18 @@ import logging
 import random
 
 class bofh(Module):
-	def cmd_bofh(self, **kwargs):
-		"""!bofh: to get a random BOFH excuse"""
-		return [self.__random_quote()]
+    def cmd_bofh(self, **kwargs):
+        """!bofh: to get a random BOFH excuse"""
+        return [self.__random_quote()]
 
-	def __random_quote(self):
-		"""Read a quote from a text file"""
-		try:
-			with open(self.get_config('quote_file'), 'rt', encoding='utf-8') as fd:
-				return 'The cause of the problem is: ' + random.choice(fd.readlines())
-		except IOError as e:
-			logging.exception('Quote IOError')
-			return 'Error: quote file not found: {}'.format(e)
-		except:
-			logging.exception('Quote Exception')
-			return 'Error: no quote file defined'
+    def __random_quote(self):
+        """Read a quote from a text file"""
+        try:
+            with open(self.get_config('quote_file'), 'rt', encoding='utf-8') as fd:
+                return 'The cause of the problem is: ' + random.choice(fd.readlines())
+        except IOError as e:
+            logging.exception('Quote IOError')
+            return 'Error: quote file not found: {}'.format(e)
+        except:
+            logging.exception('Quote Exception')
+            return 'Error: no quote file defined'
